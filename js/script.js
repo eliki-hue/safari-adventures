2805618aa4330e252ac21a0e9bf1a9a485a98fe9
Zachary Gichiri <gichirigichuru@gmail.com>
2022-01-20 16:12:47 +0300

    add images to my about part

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $("#icon1").click(function(){
+        $(this).hide()
+        $("#what1").show()
+    })
+    $("#what1").click(function(){
+        $(this).hide()
+        $("#icon1").show()
+    })
+    $("#icon2").click(function(){
+        $(this).hide()
+        $("#what2").show()
+    })
+    $("#what2").click(function(){
+        $(this).hide()
+        $("#icon2").show()
+    })
+    $("#icon3").click(function(){
+        $(this).hide()
+        $("#what3").show()
+    })
+    $("#what3").click(function(){
+        $(this).hide()
+        $("#icon3").show()
+    })
+
+
+
+})
